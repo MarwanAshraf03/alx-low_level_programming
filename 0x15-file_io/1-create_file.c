d837b738af1e19f9cf21382852b0672496dd9053
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-08-07 23:32:04 +0300

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,16 +6,16 @@
 */
 int _strlen(char *str)
 {
-    int count;
+	int count;
 
-    if (!str)
-        return (0);
-    while(*str)
-    {
-        count++;
-        str++;
-    }
-    return (count);
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	return (count);
 }
 /**
  * create_file - function that creates a file.
@@ -25,16 +25,16 @@ int _strlen(char *str)
 */
 int create_file(const char *filename, char *text_content)
 {
-    int file_d, n, len = _strlen(text_content);
+	int file_d, n, len = _strlen(text_content);
 
-    if (!filename)
-        return (-1);
-    file_d = creat(filename, 0600);
-    if (file_d == -1)
-        return (-1);
-    if((n = write(file_d, text_content, len)) < len)
-        return (-1);
-    close(file_d);
-    return (1);
-    
+	if (!filename)
+		return (-1);
+	file_d = creat(filename, 0600);
+	if (file_d == -1)
+		return (-1);
+	n = write(file_d, text_content, len);
+	if (n < len)
+		return (-1);
+	close(file_d);
+	return (1);
 }
