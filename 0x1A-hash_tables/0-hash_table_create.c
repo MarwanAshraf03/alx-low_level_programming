0101a9050d17d3345a8f001c5170e183848ad5a1
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-08 20:10:59 +0300

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,6 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *ht;
 
 	if (!size)
@@ -14,11 +15,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!ht)
 		return (NULL);
 	ht->size = size;
-	ht->array = (hash_node_t **) calloc(ht->size, sizeof(hash_node_t));
+	ht->array = (hash_node_t **) calloc(ht->size, sizeof(hash_node_t *));
 	if (!ht->array)
 	{
 		free(ht);
 		return (NULL);
 	}
+	for (i = 0; i < ht->size; i++)
+		ht->array[i] = NULL;
 	return (ht);
 }
