bf3409b23cc52fa2f0f32ce383b7541604badf9c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-09 00:46:44 +0300

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,35 +15,15 @@ void hash_table_print(const hash_table_t *ht)
 	putchar('{');
 	for (iter = 0; iter < ht->size; iter++)
 	{
-		if (iter == (ht->size - 1))
-			c = 0;
-		if (c > 0)
-			printf(", ");
-		if (!ht->array[iter])
+		h_node = ht->array[iter];
+		while (h_node)
 		{
-			c = 0;
-			continue;
+			if (c > 0)
+				printf(", ");
+			printf("'%s': '%s'", h_node->key, h_node->value);
+			h_node = h_node->next;
+			c++;
 		}
-		if (ht->array[iter]->next)
-		{
-			c = 1;
-			h_node = ht->array[iter];
-			putchar('{');
-			while (h_node)
-			{
-				if (c > 0)
-					printf(", ");
-				printf("'%s': '%s'", h_node->key, h_node->value);
-				h_node = h_node->next;
-				c++;
-			}
-			putchar('}');
-			c = 0;
-			break;
-		}
-		c = 1;
-		printf("'%s': '%s'", ht->array[iter]->key, ht->array[iter]->value);
 	}
 	printf("}\n");
 }
-
