2760b16d4b70b09652687d10ef321e562df1a422
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-08 20:09:53 +0300

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,22 +6,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
 	hash_table_t *ht;
 
 	if (!size)
 		return (NULL);
-	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+	ht = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 	ht->size = size;
-	ht->array = (hash_node_t **) calloc(ht->size, sizeof(hash_node_t));
+	ht->array = (hash_node_t **) calloc(ht->size, sizeof(hash_node_t *));
 	if (!ht->array)
 	{
 		free(ht);
 		return (NULL);
 	}
-	for (i = 0; i < ht->size; i++)
-		ht->array[i] = NULL;
 	return (ht);
 }
