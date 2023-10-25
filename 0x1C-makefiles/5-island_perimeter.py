eae8e00c031f388f298c38f2af00ebc33f599c13
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-10-26 00:17:37 +0300

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,21 +8,37 @@ def island_perimeter(grid):
     the perimeter of an island (grid)
     a 2 * 2 matrix
     """
-    # grid = grid[1:len(grid)-1]
     perimeter = 0
     add = 0
-    for i in range(1, len(grid)-1):
-        for j in range(1, len(grid[i])-1):
+    # for i in range(1, len(grid)-1):
+    #     for j in range(1, len(grid[i])-1):
+    #         add = grid[i][j]
+    #         if add == 1:
+    #             add *= 4
+    #             if grid[i][j-1] == 1:
+    #                 add -= 1
+    #             if grid[i-1][j] == 1:
+    #                 add -= 1
+    #             if grid[i][j+1] == 1:
+    #                 add -= 1
+    #             if grid[i+1][j] == 1:
+    #                 add -= 1
+    #         perimeter += add
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             add = grid[i][j]
             if add == 1:
                 add *= 4
-                if grid[i][j-1] == 1:
-                    add -= 1
-                if grid[i-1][j] == 1:
-                    add -= 1
-                if grid[i][j+1] == 1:
-                    add -= 1
-                if grid[i+1][j] == 1:
-                    add -= 1
+                try :
+                    if grid[i][j-1] == 1:
+                        add -= 1
+                    if grid[i-1][j] == 1:
+                        add -= 1
+                    if grid[i][j+1] == 1:
+                        add -= 1
+                    if grid[i+1][j] == 1:
+                        add -= 1
+                except IndexError:
+                    pass
             perimeter += add
     return perimeter
